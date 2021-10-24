9be134b6fb26bacd130a79f93c5bb55dd07dcd54
Bartlomiej <bartlomiejswiech99@gmail.com>
2021-10-25 00:17:54 +0200

    Add fontawesome and google font

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import LayoutMain from "./application/layouts/Main/LayoutMain";
 import Slider from "./application/sections/Slider/Slider";
 import ArticleSection from "./application/sections/Article/ArticleSection";
 
+import "./application/styles.css";
+import "./vendors/normalize/normalize.css";
+
 const App = () => {
   return (
     <LayoutMain>
