c7eeb5acd11d73bf14d7de703425cbd6319cff44
Bartlomiej <bartlomiejswiech99@gmail.com>
2021-10-31 21:15:44 +0100

    Implement article fetching, loader and pagination

--- src/application/sections/Article/ArticleSection.jsx
+++ src/application/sections/Article/ArticleSection.jsx
@@ -1,16 +1,40 @@
 import React, { useState, useEffect } from "react";
 import ArticleList from "./ArticleList";
+import { API } from "../../../static/definitions";
 
 const ArticleSection = () => {
   const [articles, setArticles] = useState([]);
+  const [paginator, setPaginator] = useState(1);
+  const [loader, setLoader] = useState(true);
+
+  function fetchArticles() {
+    if (paginator === false) return;
+    setLoader(() => true);
+    fetch(`${API}/articles${paginator}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch the data!");
+        }
+      })
+      .then((response) => {
+        if (response.length === 0) {
+          setPaginator(() => false);
+          setLoader(() => false);
+        } else {
+          setTimeout(() => {
+            setArticles((prevState) => [...prevState, ...response]);
+            setPaginator((prevState) => prevState + 1);
+            setLoader(() => false);
+          }, 1500);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
 
   useEffect(() => {
-    fetch("http://127.0.0.1:3050/articles?")
-      .then((response) => response.json())
-      .then((response) => {
-        setArticles((prevState) => response);
-      })
-      .catch((error) => console.log("No articles found!"));
+    fetchArticles();
   }, []);
 
   return (
@@ -18,9 +42,20 @@ const ArticleSection = () => {
       <div className="container container--xl">
         <h2 className="article-section__title">World</h2>
         <ArticleList articles={articles} />
-        <div id="article-loader" className="article-loader">
-          <div className="article-loader__spinner"></div>
-        </div>
+        {loader ? (
+          <div id="article-loader" className="article-loader">
+            <div className="article-loader__spinner"></div>
+          </div>
+        ) : (
+          ""
+        )}
+        {paginator && !loader && (
+          <div className="text-center mt-md">
+            <button className="btn btn-primary" onClick={fetchArticles}>
+              Load More
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
